6b1cb482de35a0f0c00f46cc7f34e0cb2eaa2de9
ChamiHewa <chewawitharana+1@gmail.com>
2023-10-20 19:35:15 +0800

    Adding testng.xml file

--- src/test/java/testcase/Login.java
+++ src/test/java/testcase/Login.java
@@ -4,6 +4,7 @@ import base.BaseTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
@@ -12,9 +13,9 @@ import static org.testng.Assert.assertEquals;
 
 public class Login extends BaseTest {
 
-    @Test
-    public static void loginTest() {
-        //validate the loaded page is correct by checking the web page title
+    @Test(priority=1)
+    public static void ValidatePageLoading() {
+        //Testcase 1: validate the loaded page is correct by checking the web page title
         String actual = driver.getTitle();
         String expected = "ParaBank | Welcome | Online Banking";
 
@@ -24,13 +25,15 @@ public class Login extends BaseTest {
             System.out.println("Title is similar. Test case is Pass");
         }
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-
+    }
+    @Test(priority=2)
+    public static void LoginEmptyCred() {
         //locators
         WebElement username = driver.findElement(By.xpath(loc.getProperty("username")));
         WebElement password = driver.findElement(By.xpath(loc.getProperty("password")));
         WebElement login = driver.findElement(By.xpath(loc.getProperty("login_btn")));
 
-        //Testcase 1: Validate login with empty credentials
+        //Testcase 2: Validate login with empty credentials
         //Click login button
         login.click();
         driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
@@ -40,23 +43,22 @@ public class Login extends BaseTest {
             System.out.println("Login failed with empty credentials. Test case is Pass");
         } else if (driver.findElement(By.xpath(loc.getProperty("error_msg"))).getText().contains("An internal error has occurred and has been logged.")) {
             System.out.println("An internal error has occured. Test case is Fail");
-        }else {
+        } else {
             System.out.println("Test case is Fail");
         }
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
-
-        //Testcase 2: Validate with invalid credentials
-        enterUsernameAndPassword("aaaa", "bbb", username, password);
-        invalidCredentials(driver);
-
-        //Testcase 3: Validate login with valid credentials
-        enterUsernameAndPassword("john","demo",username,password);
-        validCredentials(driver);
-        login.click();
     }
 
-    private static void invalidCredentials(WebDriver driver) {
-        //Click login button
+    @Test(priority=3)
+    public static void LoginInvalidCred() {
+        //locators
+        WebElement username = driver.findElement(By.xpath(loc.getProperty("username")));
+        WebElement password = driver.findElement(By.xpath(loc.getProperty("password")));
+        WebElement login = driver.findElement(By.xpath(loc.getProperty("login_btn")));
+
+        //Testcase 3: Validate with invalid credentials
+        enterUsernameAndPassword("aaaa", "bbb", username, password);
+
         driver.findElement(By.xpath(loc.getProperty("login_btn"))).click();
 
         driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
@@ -70,8 +72,16 @@ public class Login extends BaseTest {
         }
         driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
     }
+    @Test(priority=4)
+    @Parameters({"usernameP","passwordP"})
+    public static void LoginValidCred() {
+        WebElement username = driver.findElement(By.xpath(loc.getProperty("username")));
+        WebElement password = driver.findElement(By.xpath(loc.getProperty("password")));
+        WebElement login = driver.findElement(By.xpath(loc.getProperty("login_btn")));
 
-    private static void validCredentials(WebDriver driver) {
+        //Testcase 4: Validate login with valid credentials
+        enterUsernameAndPassword("john","demo",username,password);
+
         driver.findElement(By.xpath(loc.getProperty("login_btn"))).click();
 
         driver.manage().timeouts().pageLoadTimeout(50, TimeUnit.SECONDS);
@@ -79,18 +89,18 @@ public class Login extends BaseTest {
         if (driver.findElement(By.xpath(loc.getProperty("welcome_msg"))).getText().contains("Welcome")) {
             System.out.println("Logged in successfully with valid credentials. Test case Pass");
         } else if (driver.findElement(By.xpath(loc.getProperty("error_msg"))).getText().contains("An internal error has occurred and has been logged.")) {
-        System.out.println("An internal error has occured. Test case is Fail");
+            System.out.println("An internal error has occured. Test case is Fail");
         } else {
             System.out.println("Login fail. Testcase is Pass");
         }
     }
 
-    private static void enterUsernameAndPassword(String usernameV, String passwordV, WebElement username, WebElement password) {
+    private static void enterUsernameAndPassword(String usernameP, String passwordP, WebElement username, WebElement password) {
         username.clear();
         username.click();
-        username.sendKeys(usernameV);
+        username.sendKeys(usernameP);
         password.clear();
         password.click();
-        password.sendKeys(passwordV);
+        password.sendKeys(passwordP);
     }
 }
